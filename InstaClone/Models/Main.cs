4fd4d4c4be19c87c315908c57ca3eb996402434f
Lucas Batista <lucas.aps11@gmail.com>
2023-01-06 21:32:40 -0300

    Adicionar arquivos de projeto.

---
+++ InstaClone/Models/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InstaClone.Models
+{
+    public class Main
+    {
+        public string olucasdev { get; set; }
+
+        public Main(string usuario){
+            olucasdev = usuario;
+        }
+    }
+}
